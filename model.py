85ddcf8c9166c14edb0a32f3c323656ddee46767
Ghani-25 <54942476+Ghani-25@users.noreply.github.com>
2023-04-18 13:27:05 +0200

    Update model.py

--- model.py
+++ model.py
@@ -5,20 +5,12 @@ import pinecone
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("Ghani-25/LF_enrich_sim", device='cpu')
-pinecone.init(api_key='16146b33-2d76-4f3a-b03b-92fb81d42a42', environment='us-east4-gcp')
+pinecone.init(api_key='026cfe1e-78ef-4fbd-8398-e6dcadc030e0', environment='asia-southeast1-gcp')
 index = pinecone.Index('aiprospects')
-#url = "https://drive.google.com/uc?export=download&id=1rQc3XpyzW3a2l1m6ewqq_iBvot2sS4aY"
-#OccClean = "occupationClean5m.csv"
-#gdown.download(url, OccClean, quiet=False)
-#occupation = pd.read_csv('./occupationClean5m.csv', sep=',', lineterminator='\n', on_bad_lines='skip', header=0, encoding='UTF-8')
+
 def enrichir(query):
     xq = modell.encode([query]).tolist()
     result = index.query(xq, top_k=30, includeMetadata=False)
     res = result.to_dict() #conversion to dict
     lis = list(res.values())[0]
-    #Liste_enrichie = pd.DataFrame(lis)
-    #Liste_enrichie = Liste_enrichie.rename(columns={"id": "linkedinId"})
-    #Liste_enrichie['linkedinId'] = Liste_enrichie['linkedinId'].astype(float)
-    #merged_df = pd.merge(Liste_enrichie, occupation, on='linkedinId', how='inner')
-    #merged_df.drop_duplicates(subset='linkedinId', keep='first', inplace=True)
     return lis
