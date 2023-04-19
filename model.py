cae9535a6e502cb96d6003f265aab24839cf9f51
Ghani-25 <54942476+Ghani-25@users.noreply.github.com>
2023-04-19 15:44:50 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import torch
 import pinecone
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("Ghani-25/LF_enrich_sim", device='cpu')
 pinecone.init(api_key='026cfe1e-78ef-4fbd-8398-e6dcadc030e0', environment='asia-southeast1-gcp')
-index = pinecone.Index('aiprospects')
+index = pinecone.Index('aipros')
 
 def enrichir(query):
     xq = model.encode([query]).tolist()
