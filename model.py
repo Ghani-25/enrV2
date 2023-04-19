8f72894c23fbb0ba42690d4a2928245581e41086
Ghani-25 <54942476+Ghani-25@users.noreply.github.com>
2023-04-19 16:32:18 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import torch
 import pinecone
 from sentence_transformers import SentenceTransformer, util
 
@@ -7,8 +6,8 @@ pinecone.init(api_key='026cfe1e-78ef-4fbd-8398-e6dcadc030e0', environment='asia-
 index = pinecone.Index('aipros')
 
 def enrichir(query):
-    xq = model.encode([query]).tolist()
-    result = index.query(xq, top_k=30, includeMetadata=True)
+    xq = model.encode(query).tolist()
+    result = index.query(xq, top_k=30, includeMetadata=False)
     res = result.to_dict() #conversion to dict
     lis = list(res.values())[0]
     return lis
